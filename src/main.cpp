7d32e8a7525d572657610fe60b0cd1305742095e
arnaucresp0 <acs2000f20@gmail.com>
2023-11-23 22:16:55 +0100

    full structure code change & whatsapp bot added

--- src/main.cpp
+++ src/main.cpp
@@ -7,133 +7,199 @@
 #include <Adafruit_Sensor.h>
 #include <LittleFS.h>
 #include <ESPAsyncWebServer.h>
+#include <UrlEncode.h>
+#include <Hash.h>
 
-// Define DHT sensor and type
-#define DHTTYPE DHT11
-const int DHTPin = 5;
-DHT dht(DHTPin, DHTTYPE);
 
-// Wifi connection data
-const char* ssid = "dragino-1c0ad8";
-const char* password = "dragino-dragino";
-WiFiServer server(80);
+// Replace with your network credentials
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+//Replace with your country code (i.e. +34) and phone number
+String phoneNumber = "+34722254551"; 
+String apiKey = "1084263";
 
-// DHT data variables
-static char celsiusTemp[7];
-static char humidityTemp[7];
 
-void setup() {
-  Serial.begin(115200);
-  delay(10);
+#define DHTPIN 5     // Digital pin connected to the DHT sensor
 
-  dht.begin();
+// Uncomment the type of sensor in use:
+//#define DHTTYPE    DHT11     // DHT 11
+#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
-  // Connecting to WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+DHT dht(DHTPIN, DHTTYPE);
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+// current temperature & humidity, updated in loop()
+float t = 0.0;
+float h = 0.0;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;    // will store last time DHT was updated
+
+// Updates DHT readings every 10 seconds
+const long interval = 10000;  
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link rel="icon" href="data:,">
+<style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>ESP8266 DHT Server</h2>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+    <span class="dht-labels">Temperature</span> 
+    <span id="temperature">%TEMPERATURE%</span>
+    <sup class="units">&deg;C</sup>
+  </p>
+  <p>
+    <i class="fas fa-tint" style="color:#00add6;"></i> 
+    <span class="dht-labels">Humidity</span>
+    <span id="humidity">%HUMIDITY%</span>
+    <sup class="units">%</sup>
+  </p>
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperature").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperature", true);
+  xhttp.send();
+}, 10000 ) ;
+
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/humidity", true);
+  xhttp.send();
+}, 10000 ) ;
+</script>
+</html>)rawliteral";
+
+// Replaces placeholder with DHT values
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "TEMPERATURE"){
+    return String(t);
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  // Start the web server
-  server.begin();
-  Serial.println("Web server running. Waiting for the ESP IP...");
-  delay(10000);
-
-  // Printing the ESP IP address
-  Serial.println(WiFi.localIP());
+  else if(var == "HUMIDITY"){
+    return String(h);
+  }
+  return String();
 }
 
-void loop() {
-  WiFiClient client = server.available();
-  if (client) {
-    Serial.println("New client");
-    boolean blank_line = true;
-    while (client.connected()) {
-      //If the client is connected and available we will run all the code below:
-      if (client.available()) {
-        char c = client.read();
-
-        if (c == '\n' && blank_line) {
-          //We start the sensor readings:
-
-          //----------------DHT----------------
-          float h = dht.readHumidity();
-          float t = dht.readTemperature();
-          float f = dht.readTemperature(true);
-          //If the values ​​read are not numerical values, we send an error message:
-          if (isnan(h) || isnan(t) || isnan(f)) {
-            Serial.println("Failed to read from DHT sensor!");
-            strcpy(celsiusTemp, "Failed");
-            strcpy(humidityTemp, "Failed");
-          } else {
-            //The code performs the temperature and humidity calculation:
-            float hic = dht.computeHeatIndex(t, h, false);
-            dtostrf(hic, 6, 2, celsiusTemp);
-            dtostrf(h, 6, 2, humidityTemp);
-          }
-
-          //--------SOIL_HUMIDITY_SENSOR----------
-          
-          //
-          //----------PRESENCE_SENSOR-------------
-          
-          //
-          static char presence[7];
-
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");
-          client.println();
-
-          // Send the CSS content within a <style> tag
-          client.println("<style>");
-          String css = "";
-          //Open the CSS file in read mode:
-          File cssFile = LittleFS.open("/styles.css", "r"); //ARREGLAR SPIFFS TO LITTLEFS
-          if (cssFile) {
-            while (cssFile.available()) {
-              css += cssFile.readString();
-            }
-            cssFile.close();
-          }
-          client.print(css);
-          client.println("</style>");
-
-          // Read the HTML file into a string
-          String webpage = "";
-          File htmlFile = LittleFS.open("/webpage.html", "r"); //ARREGLAR SPIFFS TO LITTLEFS
-          if (htmlFile) {
-            while (htmlFile.available()) {
-              webpage += htmlFile.readString();
-            }
-            htmlFile.close();
-          }
-          // Replace placeholders in the HTML content with sensor data
-          webpage.replace("{{celsiusTemp}}", celsiusTemp);
-          webpage.replace("{{humidity}}", humidityTemp);
-          webpage.replace("{{presence}}", presence);
-
-          // Send the HTML content
-          client.print(webpage);
-          break;
-        }
-        if (c == '\n') {
-          blank_line = true;
-        } else if (c != '\r') {
-          blank_line = false;
-        }
-      }
-    }
-
-    delay(1);
-    client.stop();
-    Serial.println("Client disconnected.");
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  dht.begin();
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println(".");
   }
+
+  // Print ESP8266 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(t).c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(h).c_str());
+  });
+
+  // Start server
+  server.begin();
+}
+ 
+void loop(){  
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you updated the DHT values
+    previousMillis = currentMillis;
+    // Read temperature as Celsius (the default)
+    float newT = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    //float newT = dht.readTemperature(true);
+    // if temperature read failed, don't change t value
+    if (isnan(newT)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      t = newT;
+      Serial.println(t);
+    }
+    // Read Humidity
+    float newH = dht.readHumidity();
+    // if humidity read failed, don't change h value 
+    if (isnan(newH)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      h = newH;
+      Serial.println(h);
+    }
+  }
+}
+
+void sendMessage(String message){
+  // Data to send with HTTP POST
+  String url = "http://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);
+  WiFiClient client;    
+  HTTPClient http;
+  http.begin(client, url);
+
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(url);
+  if (httpResponseCode == 200){
+    Serial.print("Message sent successfully");
+  }
+  else{
+    Serial.println("Error sending the message");
+    Serial.print("HTTP response code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Free resources
+  http.end();
 }
